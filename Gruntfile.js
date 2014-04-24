af3bc33a7f6995e0f6393a7aba8f37ad01ca4d8d
Ertuğrul Emre Ertekin <ee@ertek.in>
2014-04-24 15:15:40 +0300

    Clean created files after Nodeunit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'versionify', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'versionify', 'nodeunit','clean']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
